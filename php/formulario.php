ac31882cb7d00f812dd687a938e9c2db85818f47
BENJAMIN FELIPE VARGAS ZUNIGA <BENJAMIN.VARGASZ@correoaiep.cl>
2022-07-31 21:20:19 -0400

    Se centraron los campos a excepción de los botones

--- php/formulario.php
+++ php/formulario.php
@@ -12,15 +12,17 @@
   <div class="formulario_pagina">
 
   
-        <div class="column is-3">
-          <label class="label">Rut Empresa</label>
-          <div class="control">
-            <input class="input" type="text" placeholder="Ingrese el rut de la empresa">
+        <div class="columns is-centered"><!-- Para centrar los campos -->
+          <div class="column is-3"><!-- Para que los campos no se vean tan amplios-->
+            <label class="label">Rut Empresa</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Ingrese el rut de la empresa">
+            </div>
           </div>
         </div>
 
-        <div class="columns is-centered">
-          <div class="column is-3">
+        <div class="columns is-centered"><!-- Para centrar los campos -->
+          <div class="column is-3"><!-- Para que los campos no se vean tan amplios-->
             <label class="label">Nombre de la empresa</label>
             <div class="control has-icons-left has-icons-right">
               <input class="input is-success" type="text" placeholder="Ingrese el Nombre de la empresa" value="">
@@ -34,71 +36,84 @@
           </div>
         </div>
 
-        <div class="column is-3">
+        <div class="columns is-centered"><!-- Para centrar los campos -->
+          <div class="column is-3"><!-- Para que los campos no se vean tan amplios-->
           <label class="label">Correo a la cual se la mandara la info</label>
-          <div class="control has-icons-left has-icons-right">
-            <input class="input is-danger" type="email" placeholder="Ingrese Correo" value="">
-            <span class="icon is-small is-left">
+            <div class="control has-icons-left has-icons-right">
+              <input class="input is-danger" type="email" placeholder="Ingrese Correo" value="">
+              <span class="icon is-small is-left">
               <i class="fas fa-envelope"></i>
-            </span>
-            <span class="icon is-small is-right">
+              </span>
+              <span class="icon is-small is-right">
               <i class="fas fa-exclamation-triangle"></i>
-            </span>
+              </span>
+            </div>
+            <p class="help is-danger">Correo no es Valido</p>
           </div>
-          <p class="help is-danger">Correo no es Valido</p>
         </div>
 
-        <div class="column is-3">
-          <label class="label">Seleccione su Region</label>
-          <div class="control">
-            <div class="select">
-              <select>
+        <div class="columns is-centered"><!-- Para centrar los campos -->
+          <div class="column is-3"><!-- Para que los campos no se vean tan amplios-->
+            <label class="label">Seleccione su Region</label>
+              <div class="control">
+                <div class="select">
+                <select>
                 <option>Tarapacá</option>
                 <option>O'Higgins</option>
-              </select>
+                </select>
+              </div>
             </div>
           </div>
         </div>
 
 
-        <div class="column is-3">
-          <label class="label">Comuna:</label>
-          <div class="control">
-            <input class="input" type="text" placeholder="Ingrese su Comuna">
+        <div class="columns is-centered"><!-- Para centrar los campos -->
+          <div class="column is-3"><!-- Para que los campos no se vean tan amplios-->
+            <label class="label">Comuna:</label>
+            <div class="control">
+              <input class="input" type="text" placeholder="Ingrese su Comuna">
+            </div>
+          </div>
+        </div>
+
+        <div class="columns is-centered"><!-- Para centrar los campos -->
+          <div class="column is-3"><!-- Para que los campos no se vean tan amplios-->
+            <label class="label">Mensaje</label>
+            <div class="control">
+              <textarea class="textarea" placeholder="Adjunte su Mensaje"></textarea>
+            </div>
+          </div>
+        </div>
+
+        <div class="columns is-centered"><!-- Para centrar los campos -->
+          <div class="column is-3"><!-- Para que los campos no se vean tan amplios-->
+            <div class="control">
+              <label class="checkbox">
+                <input type="checkbox">
+               Acepto los <a href="#">términos y condiciones.</a>
+              </label>
+            </div>
+          </div>
+        </div>  
+
+        <div class="columns is-centered"><!-- Para centrar los campos -->
+          <div class="column is-3"><!-- Para que los campos no se vean tan amplios-->
+            <div class="control">
+              <label class="radio">
+                <input type="radio" name="question">
+                Si
+                </label>
+                <label class="radio">
+                <input type="radio" name="question">
+                No
+              </label>
+            </div>
           </div>
         </div>
 
 
-        <div class="column is-3">
-          <label class="label">Mensaje</label>
-          <div class="control">
-            <textarea class="textarea" placeholder="Adjunte su Mensaje"></textarea>
-          </div>
-        </div>
-
-        <div class="column is-3">
-          <div class="control">
-            <label class="checkbox">
-              <input type="checkbox">
-              acepta las condiciones <a href="#">Terminos y condiciones</a>
-            </label>
-          </div>
-        </div>
-
-        <div class="column is-3">
-          <div class="control">
-            <label class="radio">
-              <input type="radio" name="question">
-              si
-            </label>
-            <label class="radio">
-              <input type="radio" name="question">
-              no
-            </label>
-          </div>
-        </div>
-
-        <div class="column is-3">
+        <!-- Falta centrar esta parte correctamente. -->
+        <div class="columns is-vcentered">
           <div class="control">
             <button class="button is-link">Enviar Formulario</button>
           </div>
